c89ffca673e9b3872071882f4ab059e4e4bf2ab2
Lily-mac <Learner-Bee@github.com>
2017-07-15 18:57:51 +0800

    init

--- selenium-test/src/com/lily/seleniumdemo/FirstDemo.java
+++ selenium-test/src/com/lily/seleniumdemo/FirstDemo.java
@@ -2,24 +2,30 @@ package com.lily.seleniumdemo;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstDemo {
 
-	public static void main(String[] args) {
-		
-		try{
-			Thread.sleep(3000);
-		}catch(InterruptedException e){
-			e.printStackTrace();
-		}
-		WebDriver driver=new FirefoxDriver();
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "/Users/Lily/Dev/selenium-2.42.2/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.baidu.com/");
+
+		// Thread.sleep(5*1000);
+
 		driver.findElement(By.id("kw")).sendKeys("hello selenium");
 		driver.findElement(By.id("su")).click();
-		
-		driver.quit();//ÍË³öËùÓÐwebdriver´°¿Ú
-		//driver.close(); Ö»¹Ø±Õµ±Ç°Ò³Ãæ
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// driver.quit();//é€€å‡º
+		// driver.close(); //å…³é—­
 
 	}
 
