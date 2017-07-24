5e45c7ebf7e7032142c236d49e102fc5e421716b
Learner-Bee <lilyboo@foxmail.com>
2017-07-24 17:59:51 +0800

    add testNG

---
+++ selenium-test/src/com/lily/seleniumdemo/NewTestNG.java
@@ -0,0 +1,22 @@
+package com.lily.seleniumdemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTestNG {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	  WebDriver dr=new ChromeDriver();
+	  dr.get("https://www.baidu.com/");
+	  WebElement element=dr.findElement(By.id("kw"));
+	  element.sendKeys("testNG");
+	  assert element.getText().contains("testNG");
+	  dr.findElement(By.id("su")).click();
+	  System.out.println("OK");
+	  
+  }
+}
