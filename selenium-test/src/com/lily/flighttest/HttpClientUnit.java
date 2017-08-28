eccd21df0ac0b1189d2232e4588392f53eea98eb
Lily-mac <Learner-Bee@github.com>
2017-08-28 22:28:41 +0800

    add httpclient

--- selenium-test/src/com/lily/flighttest/HttpClientUnit.java
+++ selenium-test/src/com/lily/flighttest/HttpClientUnit.java
@@ -4,6 +4,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 public class HttpClientUnit {
 	public static void httpClientPost(){
+		// 创建“浏览器客户端”。
 		HttpClientBuilder httpclientbuilder= HttpClientBuilder.create();
 		
 	}
---
+++ selenium-test/src/com/lily/flighttest/JsonUtil.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.lily.flighttest;
+
+
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import net.sourceforge.htmlunit.corejs.javascript.tools.debugger.Main;
+
+/**
+ * 
+ * <pre>
+ * 
+ * {
+    "result":[
+        {
+            "No":"123XXX",
+            "Date":"20170828",
+            "SA":"Beijing",
+            "DA":"Shanghai"
+        },
+        {
+            "No":"124XXX",
+            "Date":"20170828",
+            "SA":"Beijing",
+            "DA":"Shanghai"
+        },
+        {
+            "No":"125XXX",
+            "Date":"20170828",
+            "SA":"Beijing",
+            "DA":"Shanghai"
+        }
+    ]
+}
+
+ * </pre>
+ * 
+ * @author Lily
+ *
+ */
+public class JsonUtil {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String originalStr = "{\"result\":[{\"No\":\"123XXX\",\"Date\":\"20170828\",\"SA\":\"Beijing\",\"DA\":\"Shanghai\"},{\"No\":\"124XXX\",\"Date\":\"20170828\",\"SA\":\"Beijing\",\"DA\":\"Shanghai\"},{\"No\":\"125XXX\",\"Date\":\"20170828\",\"SA\":\"Beijing\",\"DA\":\"Shanghai\"}]}";
+		JSONObject jsonObject = (JSONObject) JSONObject.parse(originalStr);
+		JSONArray jsonArray= (JSONArray) jsonObject.get("result");
+		JSONObject firsrObject = (JSONObject) jsonArray.get(0);
+		String flightNo = firsrObject.get("No").toString();
+		Assert.assertEquals("╤оят", "123XXX", flightNo);
+		
+//		for (Object object : jsonArray) {
+//			JSONObject json = (JSONObject) object;
+//			String actural = (String) json.get("Date");
+//			Assert.assertEquals("╤оят", "20170828", actural, delta);
+//		}
+		
+	}
+
+}
